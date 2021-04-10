88491909f8297d24f7e653153090368c48550e3c
ito <basketball-y.0130@docomo.ne.jp>
2021-04-10 15:01:50 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          hello, <a href="https://nextjs.org">Next.js!</a>
+          H
+          ello, <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
